89a658ba41e47abf31cc066b7dcb93f3bbfbd9da
Samuel Natã <samuelnatan98@gmail.com>
2023-08-02 16:24:33 -0300

    vamo

--- src/configs/upload.js
+++ src/configs/upload.js
@@ -4,7 +4,7 @@ const crypto = require("crypto");
 
 // Uploaded files location
 const TMP_FOLDER = path.resolve(__dirname, "..", "tmp");
-const UPLOADS_FOLDER = path.resolve(TMP_FOLDER, "upload");
+const UPLOADS_FOLDER = path.resolve(TMP_FOLDER, "uploads");
 
 const MULTER = {
     storage: multer.diskStorage({
